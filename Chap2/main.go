8ed32132b45885aba0c8fd01fffc4d67519b2648
Himanshu Mactavish <Himanshu.Mactavish@gmail.com>
2022-01-22 11:20:44 +0530

    Doing Chapter2

---
+++ Chap2/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var str = "Greetings and \n\"Salutations\""
+	fmt.Println(str)
+	fmt.Println("Chapter 2 Code practice")
+	var str1 = `Greetings and "Salutatations"`
+	fmt.Println(str1)
+	fmt.Println(len(str1))
+
+}
